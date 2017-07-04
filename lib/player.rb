73ff666b3c041ebeefc0ecbb87d2f2b2a772f054
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-07-04 22:05:25 +1000

    add get_input, check_player_guess, display and player class - not working just yet

---
+++ lib/player.rb
@@ -0,0 +1,22 @@
+
+class Player
+  attr_accessor :current_guess
+
+  def get_input
+    @current_guess = ""
+    until is_valid?(@current_guess)
+      puts "Please type a single letter: "
+      @current_guess = gets.downcase.chomp
+    end
+    @current_guess    
+  end
+
+  def is_valid?(input)
+    if input.class == String && input.length == 1
+      true
+    else
+      false
+    end
+  end
+
+end
